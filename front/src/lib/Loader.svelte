df523a46383e9fe7d8fdf4057383432c042b24d1
bledda <bledda@student.42nice.fr>
2023-02-25 22:21:43 +0100

    update

---
+++ front/src/lib/Loader.svelte
@@ -0,0 +1,47 @@
+<h1>Uploading...</h1>
+
+<div class="progress">
+	<div class="progress1">
+	</div>
+</div>
+
+<style>
+	h1 {
+		font-style: normal;
+		font-weight: 500;
+		font-size: 18px;
+		line-height: 27px;
+		letter-spacing: -0.035em;
+		color: #4F4F4F;
+	}
+
+	.progress {
+		margin-top: 31px;
+		width: 100%;
+		height: 6px;
+		background: #F2F2F2;
+		border-radius: 8px;
+		position: relative;
+		margin-bottom: 7px;
+	}
+
+	.progress1 {
+		background: #2F80ED;
+		border-radius: 8px;
+		width: 100px;
+		height: 6px;
+		animation: linear 1s infinite alternate move;
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+
+	@keyframes move {
+		0% {
+			left: 0;
+		}
+		100% {
+			left: calc(100% - 100px);
+		}
+	}
+</style>
